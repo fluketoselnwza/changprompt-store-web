039dad4fa4077f2c728364e9851b2e0a580b8aac
Kiattisak Sonthong <ext.kiattisak.son@truedigital.com>
2025-01-05 17:13:48 +0700

    add form login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,10 +2,24 @@ import WelcomeImage from "@/assets/images/welcome.png";
 import { Button } from "@/components/ui/button";
 import { CustomInput, CustomInputIcon } from "./components";
 import CloseEyeIcon from "@/assets/icons/icon-close-eye.png";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-const LoginPage = () => {
+type Inputs = {
+  phoneNumber?: string;
+  password?: string;
+};
+
+const LoginPage: React.FC = () => {
   const [closePassword, setClosePassword] = useState<boolean>(true);
+  const [errorMsg, setErrorMsg] = useState<string>("");
+
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log("data --* ", data);
+    setErrorMsg("เบอร์โทรศัพท์หรือรหัสผ่านไม่ถูกต้อง");
+  };
 
   return (
     <div className="grid grid-cols-2 h-screen p-4">
@@ -37,30 +51,38 @@ const LoginPage = () => {
           />
           <span className="font-semibold text-3xl">CHANGPROMPT</span>
         </div>
-        <div className="bg-white w-full shadow-md rounded-[30px] flex flex-col items-center justify-center p-12 gap-5">
-          <p className="font-bold text-2xl">เข้าสู่ระบบจัดการข้อมูลร้านค้า</p>
-          <p className="text-[#A0A0A0]">
-            Welcome back! Please enter your datails
-          </p>
-          <div className="w-full">
-            <CustomInput
-              name="phoneNumber"
-              label="เบอร์โทรศัพท์"
-              placeholder="กรอกเบอร์โทรศัพท์"
-            />
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+          <div className="bg-white w-full shadow-md rounded-[30px] flex flex-col items-center justify-center p-12 gap-5">
+            <p className="font-bold text-2xl">เข้าสู่ระบบจัดการข้อมูลร้านค้า</p>
+            <p className="text-[#A0A0A0]">
+              Welcome back! Please enter your datails
+            </p>
+            <div className="w-full">
+              <CustomInput
+                name="phoneNumber"
+                label="เบอร์โทรศัพท์"
+                placeholder="กรอกเบอร์โทรศัพท์"
+                register={register("phoneNumber")}
+              />
+            </div>
+            <div className="w-full">
+              <CustomInputIcon
+                name="password"
+                label="รหัสผ่าน"
+                type={closePassword ? "password" : "text"}
+                placeholder="รหัสผ่าน"
+                register={register("password")}
+                iconRight={CloseEyeIcon}
+                rightOnclick={() => setClosePassword(!closePassword)}
+              />
+            </div>
+            {errorMsg && <p className="text-red-600">{errorMsg}</p>}
+            <Button className="w-full rounded-[14px]">เข้าสู่ระบบ</Button>
+            <Button variant={"link"} type="button">
+              <p>ลืมรหัสผ่าน ?</p>
+            </Button>
           </div>
-          <div className="w-full">
-            <CustomInputIcon
-              name="password"
-              label="รหัสผ่าน"
-              type={closePassword ? "password" : "text"}
-              placeholder="รหัสผ่าน"
-              iconRight={CloseEyeIcon}
-              rightOnclick={() => setClosePassword(!closePassword)}
-            />
-          </div>
-          <Button className="w-full rounded-[14px]">เข้าสู่ระบบ</Button>
-        </div>
+        </form>
       </div>
     </div>
   );
