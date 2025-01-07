ec3e6efbb281c4af1b6f6d057a44eb5060d2afb1
Kiattisak Sonthong <ext.kiattisak.son@truedigital.com>
2025-01-07 23:09:44 +0700

    sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from "react"
 import {
   AudioWaveform,
   BookOpen,
@@ -10,10 +10,19 @@ import {
   PieChart,
   Settings2,
   SquareTerminal,
-} from "lucide-react";
+} from "lucide-react"
 
-import { NavMain } from "@/components/nav-main";
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import { NavMain } from "@/components/nav-main"
+import { NavProjects } from "@/components/nav-projects"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
 
 // This is sample data.
 const data = {
@@ -143,14 +152,22 @@ const data = {
       icon: Map,
     },
   ],
-};
+}
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
-      <SidebarContent className="bg-white">
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
         <NavMain items={data.navMain} />
+        <NavProjects projects={data.projects} />
       </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
     </Sidebar>
-  );
+  )
 }
--- src/pages/sidebar-layout.tsx
+++ src/pages/sidebar-layout.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-// import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "./app-sidebar";
 import HeaderBar from "./header-bar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 interface SidebarLayoutProps {
   children: React.ReactNode;
@@ -11,13 +14,19 @@ interface SidebarLayoutProps {
 const SidebarLayout: React.FC<SidebarLayoutProps> = ({ children }) => {
   return (
     <>
-      <HeaderBar />
       <SidebarProvider>
-        <AppSidebar />
-        <main>
-          {/* <SidebarTrigger /> */}
-          {children}
-        </main>
+        <HeaderBar />
+        <AppSidebar className="mt-[56px]" />
+        <SidebarInset className="mt-[56px]">
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+
+              {/* <Separator orientation="vertical" className="mr-2 h-4" /> */}
+            </div>
+          </header>
+          <div>{children}</div>
+        </SidebarInset>
       </SidebarProvider>
     </>
   );
