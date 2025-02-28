e086ff8fa8450df37162c30066af5c2140ee2c77
Kiattisak Sonthong <ext.kiattisak.son@truedigital.com>
2025-02-28 23:33:42 +0700

    fix role code

--- src/pages/data/role-code.ts
+++ src/pages/data/role-code.ts
@@ -1,26 +1,14 @@
 export const ROLE_CODE = [
   {
-    label: "แอดมินระดับสูง",
-    value: "S-ADMIN",
-  },
-  {
-    label: "แอดมิน",
-    value: "ADMIN",
-  },
-  {
     label: "เจ้าของร้าน",
     value: "OWN",
   },
   {
     label: "พนักงานธุรการ",
-    value: "ADMIN2",
+    value: "ADMIN",
   },
   {
     label: "พนักงานขาย",
     value: "SALE",
-  },
-  {
-    label: "ช่าง",
-    value: "TECH",
   },
 ];
